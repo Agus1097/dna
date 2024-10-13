e642f705c8bfc0e47f2081513cefe0b57700f3ef
Agustin Leyes <agustinleyes10@gmail.com>
2024-10-13 00:30:41 -0300

    change POST for GET

--- src/main/java/com/example/inicial1/controllers/StatsController.java
+++ src/main/java/com/example/inicial1/controllers/StatsController.java
@@ -4,7 +4,7 @@ import com.example.inicial1.services.StatsService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +15,7 @@ public class StatsController {
 
     private final StatsService statsService;
 
-    @PostMapping
+    @GetMapping
     public ResponseEntity<?> getStats() {
         try {
             return ResponseEntity.status(HttpStatus.OK).body(statsService.getStats());
